b789612e13a35fda07c4529a00c48ac9f57a916c
Robert W. Leonard <robert@writerrollcall.com>
2022-02-13 18:47:57 -0600

    Set up DB context

---
+++ BlazorTrails.Api/Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorTrails.Api.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTrails.Api.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Trail> Trails => Set<Trail>();
+        public DbSet<RouteInstruction> RouteInstructions => Set<RouteInstruction>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new TrailConfig());
+            modelBuilder.ApplyConfiguration(new RouteInstructionConfig());
+        }
+    }
+}
